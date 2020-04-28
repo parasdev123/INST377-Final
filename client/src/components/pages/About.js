2b22ae9497994fb9c5fce79453233804b02f71af
parasdev123 <45490064+parasdev123@users.noreply.github.com>
2020-04-28 18:45:03 -0400

    added info and cards for the about page

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,10 +1,72 @@
 import React from "react";
-// import { Card, Container, Row, Col, CardGroup } from "react-bootstrap";
+import { Card, CardDeck } from "react-bootstrap";
 
 const About = () => {
   return (
     <div>
-      <h1>About Page</h1>
+      <br />
+      <br />
+      <CardDeck>
+        <Card border="dark">
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Dark Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card border="dark">
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Dark Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card border="dark">
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Dark Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </CardDeck>
+      <br />
+      <br />
+      <div
+        style={{ width: "52rem", marginLeft: "auto", marginRight: "auto" }}
+        class="lower-card"
+      >
+        <CardDeck>
+          <Card border="dark" style={{ width: "18rem" }}>
+            <Card.Header>Header</Card.Header>
+            <Card.Body>
+              <Card.Title>Dark Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>{" "}
+          <Card border="dark" style={{ width: "18rem" }}>
+            <Card.Header>Header</Card.Header>
+            <Card.Body>
+              <Card.Title>Dark Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </CardDeck>
+      </div>
     </div>
   );
 };
