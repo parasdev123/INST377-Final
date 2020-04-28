d6ce79132c89b3c772505595f3d6d435e33860cf
parasdev123 <45490064+parasdev123@users.noreply.github.com>
2020-04-28 18:15:25 -0400

    added text cards to home page

---
+++ client/src/components/Cards.js
@@ -0,0 +1,139 @@
+import React from "react";
+import { Card, CardGroup, Accordion, Button } from "react-bootstrap";
+
+// Replace Lorem words with facts and imfo about each services
+const Cards = () => {
+  return (
+    <div>
+      <Card className="text-center" border="light">
+        <Card.Header>
+          <h4>
+            <strong>Public Services Avaliable to PG County</strong>
+          </h4>
+        </Card.Header>
+        <br />
+        <CardGroup>
+          <Card border="light">
+            <div className="card-image">
+              {/* <Card.Img variant="top" src={design} alt="design image" />  */}
+            </div>
+            <Card.Body>
+              <Card.Title className="title">Police Station</Card.Title>
+              <Card.Text>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+                suscipit ab labore, pariatur animi, laudantium, repudiandae
+                ipsam quaerat libero nemo amet accusantium. Expedita deleniti a
+                tempora repellat omnis ex cumque?
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card border="light">
+            <div className="card-image">
+              {/* <Card.Img variant="top" src={design} alt="design image" /> */}
+            </div>
+            <Card.Body>
+              <Card.Title className="title">Fire Station</Card.Title>
+              <Card.Text>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Veritatis dolore eius, laborum est sequi deleniti ea vitae
+                tempora laudantium, quasi, amet dolorum aperiam! Tenetur
+                laboriosam qui est maxime labore ipsa.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <br />
+          <br />
+          <br />
+          <Card border="light">
+            <div className="card-image">
+              {/* <Card.Img variant="top" src={build} alt="build image" /> */}
+            </div>
+            <Card.Body>
+              <Card.Title className="title">Libraries</Card.Title>
+              <Card.Text>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A
+                cupiditate veritatis iste nostrum ducimus dolor repudiandae
+                officia. Suscipit, itaque error reiciendis ad, adipisci
+                doloribus numquam natus ex veniam quod quos.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card border="light">
+            <div className="card-image">
+              {/* <Card.Img variant="top" src={launch} alt="Launch image" /> */}
+            </div>
+            <Card.Body>
+              <Card.Title className="title">Rec Centers</Card.Title>
+              <Card.Text>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+                similique nam veritatis dolor maxime, ullam architecto incidunt
+                laudantium sint quibusdam molestias ea corporis numquam
+                voluptates iste, optio, earum debitis pariatur.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </CardGroup>
+      </Card>
+      <br />
+      <br />
+      <Card>
+        <Card.Body>
+          <h1>Talk about what we want. </h1> Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. At, praesentium repudiandae, beatae quia
+          consequuntur ratione debitis sequi quam dicta cumque iste asperiores
+          officia. Magni aliquid repellat sed id praesentium vel. Lorem ipsum
+          dolor sit amet, consectetur adipisicing elit. Quod eligendi ipsa
+          consequuntur sed excepturi dolor,
+        </Card.Body>
+      </Card>
+      <br />
+      <br />
+      <div>
+        <Accordion defaultActiveKey="0">
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                Police Station
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>Map of Police Station</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                Fire Station
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="1">
+              <Card.Body>Map of Fire Station</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as={Button} variant="link" eventKey="2">
+                Libraries
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="2">
+              <Card.Body>Map of Libraries</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as={Button} variant="link" eventKey="3">
+                Rec Centers
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="3">
+              <Card.Body>Map of Rec Centers</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,28 +1,15 @@
 import React from "react";
-// import { Container, Row, Col, Card } from "react-bootstrap";
+import Cards from "../Cards";
 
 const Home = () => {
   return (
-    <div class="ui items">
-      <div class="item">
-        <a class="ui small image">
-          <img />
-        </a>
-        <div class="content">
-          <a class="header">Cute Dog</a>
-          <div class="description">
-            <p>
-              Cute dogs come in a variety of shapes and sizes. Some cute dogs
-              are cute for their adorable faces, others for their tiny stature,
-              and even others for their massive size.
-            </p>
-            <p>
-              Many people also have their own barometers for what makes a cute
-              dog.
-            </p>
-          </div>
-        </div>
-      </div>
+    <div>
+      <br />
+      <br />
+      <h1>Add a image or a banner here</h1>
+      <br />
+      <br />
+      <Cards />
     </div>
   );
 };
