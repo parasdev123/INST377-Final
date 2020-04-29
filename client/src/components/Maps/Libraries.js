fe1736205007e039c2e451cb2c49ed7d35f43e2f
parasdev123 <45490064+parasdev123@users.noreply.github.com>
2020-04-29 10:00:58 -0400

    fixed the maps

--- client/src/components/Maps/Libraries.js
+++ client/src/components/Maps/Libraries.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ReactMapGL from "react-map-gl";
+import { Container, Row, Col } from "react-bootstrap";
 
 // Followed a toturial from Leigh Halliday youtube channel
 export default function Libraries() {
@@ -7,20 +8,28 @@ export default function Libraries() {
     latitude: 38.9864713,
     longitude: -76.9562744,
     zoom: 12,
-    width: "100vw",
+    width: "70vw",
     height: "75vh",
   });
 
   return (
     <div>
-      <ReactMapGL
-        {...viewport}
-        mapboxApiAccessToken="pk.eyJ1IjoicGFyYXNkZXYiLCJhIjoiY2s5a3E2bXA5MDN0NjNscG1yejh2aXNhcSJ9._xNqZGODCSdwe4TxxWxNpA"
-        mapStyle="mapbox://styles/parasdev/ck9kqpmc40bco1iol9b5eme8j"
-        onViewportChange={(viewport) => {
-          setViewport(viewport);
-        }}
-      ></ReactMapGL>
+      <Container>
+        <Row>
+          <Col xs={6}>
+            <br />
+            <br />
+            <ReactMapGL
+              {...viewport}
+              mapboxApiAccessToken="pk.eyJ1IjoicGFyYXNkZXYiLCJhIjoiY2s5a3E2bXA5MDN0NjNscG1yejh2aXNhcSJ9._xNqZGODCSdwe4TxxWxNpA"
+              mapStyle="mapbox://styles/parasdev/ck9kqpmc40bco1iol9b5eme8j"
+              onViewportChange={(viewport) => {
+                setViewport(viewport);
+              }}
+            ></ReactMapGL>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
