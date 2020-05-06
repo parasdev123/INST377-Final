988fbfd73ef29cba8326d2faea4157417e913c99
Eltayeb <elt.abdelrahman@gmail.com>
2020-05-06 07:26:13 -0400

    Update server.js
    successfully recieved, cleaned, and send fire stations data to the front end

--- server.js
+++ server.js
@@ -136,3 +136,37 @@ function processDataForFrontEndPoliceStations(req, res) {
 app.get("/policeStations", (req, res) => {
   processDataForFrontEndPoliceStations(req, res);
 });
+
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+// new api fetch for fire stations
+
+function processDataForFrontEndFireStations(req, res) {
+  const baseURL =
+    "https://data.princegeorgescountymd.gov/resource/bzf2-94qx.json";
+
+  // Your Fetch API call starts here
+  fetch(baseURL)
+    .then((data) => data.json())
+    .then((data) => {
+      const refined = data.map((m) => ({
+        name: m.station_name,
+        lat: m.location_1.latitude,
+        long: m.location_1.longitude,
+      }));
+      // console.log(refined);
+      return refined;
+    })
+    .then((data) => {
+      console.log(data);
+      res.send({ data: data }); // here's where we return data to the front end
+    });
+}
+
+// This is our first route on our server.
+// To access it, we can use a "GET" request on the front end
+// by typing in: localhost:5500/fireStations or 127.0.0.1:5500/fireStations
+app.get("/fireStations", (req, res) => {
+  processDataForFrontEndFireStations(req, res);
+});
