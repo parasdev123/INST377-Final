a89fb0c10507908c6b16fc6f425f53b680b03860
Eltayeb <elt.abdelrahman@gmail.com>
2020-05-06 07:33:19 -0400

    Update server.js
    successfully recived, cleaned, and sent libraries data to the front end

--- server.js
+++ server.js
@@ -170,3 +170,37 @@ function processDataForFrontEndFireStations(req, res) {
 app.get("/fireStations", (req, res) => {
   processDataForFrontEndFireStations(req, res);
 });
+
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+// new api fetch for libraries
+
+function processDataForFrontEndLibraries(req, res) {
+  const baseURL =
+  "https://data.princegeorgescountymd.gov/resource/7k64-tdwr.json";
+
+  // Your Fetch API call starts here
+  fetch(baseURL)
+    .then((data) => data.json())
+    .then((data) => {
+      const refined = data.map((m) => ({
+        name: m.branch_name,
+        lat: m.location_1.latitude,
+        long: m.location_1.longitude,
+      }));
+      // console.log(refined);
+      return refined;
+    })
+    .then((data) => {
+      console.log(data);
+      res.send({ data: data }); // here's where we return data to the front end
+    });
+}
+
+// This is our first route on our server.
+// To access it, we can use a "GET" request on the front end
+// by typing in: localhost:5500/libraries or 127.0.0.1:5500/libraries
+app.get("/libraries", (req, res) => {
+  processDataForFrontEndLibraries(req, res);
+});
