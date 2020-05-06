bb061942ce720bbf360f64f9876d1b4316b4e24c
Eltayeb <elt.abdelrahman@gmail.com>
2020-05-06 07:20:22 -0400

    Update server.js
    successfully recived, cleaned, and sent police station data to the front end

--- server.js
+++ server.js
@@ -102,3 +102,37 @@ app.get("/api", (req, res) => {
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+// new api fetch for police stations
+
+function processDataForFrontEndPoliceStations(req, res) {
+  const baseURL =
+    "https://data.princegeorgescountymd.gov/resource/qkn8-5mhu.json";
+
+  // Your Fetch API call starts here
+  fetch(baseURL)
+    .then((data) => data.json())
+    .then((data) => {
+      const refined = data.map((m) => ({
+        name: m.station_name,
+        lat: m.station_address.latitude,
+        long: m.station_address.longitude,
+      }));
+      // console.log(refined);
+      return refined;
+    })
+    .then((data) => {
+      console.log(data);
+      res.send({ data: data }); // here's where we return data to the front end
+    });
+}
+
+// This is our first route on our server.
+// To access it, we can use a "GET" request on the front end
+// by typing in: localhost:5500/api or 127.0.0.1:5500/api
+app.get("/policeStations", (req, res) => {
+  processDataForFrontEndPoliceStations(req, res);
+});
