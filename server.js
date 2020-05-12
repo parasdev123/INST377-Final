c444b7cdae03e90c5aeafe64c83d56a1d9578294
parasdev123 <45490064+parasdev123@users.noreply.github.com>
2020-05-12 18:14:35 -0400

    launching to heroku, fixing other pages

--- server.js
+++ server.js
@@ -33,6 +33,9 @@ app.use(express.static("public"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
