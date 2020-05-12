9a5ca32808fa983047d3775fa8614df0e7a923ba
parasdev123 <45490064+parasdev123@users.noreply.github.com>
2020-05-12 18:32:39 -0400

    launching to heroku, fixing other pages

--- server.js
+++ server.js
@@ -36,6 +36,12 @@ if (process.env.NODE_ENV === "production") {
   app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
+  app.get("/pages/about", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+  app.get("/pages/contactus", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
