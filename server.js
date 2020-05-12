a23dbb09cb8681fa3c8a769bfc6a19e484a34364
parasdev123 <45490064+parasdev123@users.noreply.github.com>
2020-05-12 10:32:09 -0400

    changing * to / in app.get

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ const path = require("path");
 
 // Here we instantiate the server we're going to turn on
 const app = express();
-app.use(cors());
+// app.use(cors());
 
-app.get("/products/:id", function (req, res, next) {
-  res.json({ msg: "This is CORS-enabled for all origins!" });
-});
+// app.get("/products/:id", function (req, res, next) {
+//   res.json({ msg: "This is CORS-enabled for all origins!" });
+// });
 
-app.listen(80, function () {
-  console.log("CORS-enabled web server listening on port 80");
-});
+// app.listen(80, function () {
+//   console.log("CORS-enabled web server listening on port 80");
+// });
 // Servers are often subject to the whims of their environment.
 // Here, if our server has a PORT defined in its environment, it will use that.
 // Otherwise, it will default to port 5500
