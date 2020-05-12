a1c57f3b0554d96d2f9b5fee762fe276bf7393a2
parasdev123 <45490064+parasdev123@users.noreply.github.com>
2020-05-12 18:27:45 -0400

    launching to heroku, fixing other pages

--- server.js
+++ server.js
@@ -27,20 +27,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // And the ability to serve some files publicly, like our HTML.
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 // dont delete, serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
